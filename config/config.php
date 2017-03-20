f0953beae6a743eb11fce84fd289a4bb7c88a1cc
Kieran <bmxstreet@gmx.net>
2017-03-20 10:40:09 +0100

    Updated settings

--- config/config.php
+++ config/config.php
@@ -3,9 +3,8 @@
 $config = array(); //Creates Config Array
 $config["Username"] = "name"; //Query Login tsAGB
 $config["Password"] = "password"; //Query Password m8bLJwEi
-$config["IP"] = "213.202.206.203"; //Server IP/Domain
-$config["Port"] = "10150"; //Server Port
+$config["IP"] = "5.175.16.178"; //Server IP/Domain
+$config["Port"] = "9987"; //Server Port
 $config["qPort"] = "10011"; // Query Port, Default 10011
-$config["Nickname"] = rawurlencode("Furries-UnitedAGB".rand(1,99)); // Convertes Botname to a URI friendly Format
+$config["Nickname"] = rawurlencode("GroupBot".rand(1, 99)); // Random number & Convertes Botname to a URI friendly Format
 $config["timezone"] = 'Europe/Berlin';
-?>
